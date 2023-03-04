043ed6406073de374fae4bc963c623f1ced8acba
ferdyciputra <ferdyarmstrong@gmail.com>
2023-03-04 22:39:41 +0700

    edit targetElementName on file LoginPage

--- src/test/java/swaglabs/pageobjects/LoginPage.java
+++ src/test/java/swaglabs/pageobjects/LoginPage.java
@@ -7,6 +7,6 @@ import net.serenitybdd.screenplay.ui.PageElement;
 public class LoginPage extends PageObject {
     public static Target USER_NAME_FIELD = Target.the("user name field").locatedBy("#user-name");
     public static Target PASSWORD_FIELD = Target.the("password field").locatedBy("#password");
-    public static Target LOGIN_BUTTON = Target.the("password field").locatedBy("#login-button");
+    public static Target LOGIN_BUTTON = Target.the("login button").locatedBy("#login-button");
     public static Target ERROR_MESSAGE = PageElement.locatedBy(".error-message-container");
 }
